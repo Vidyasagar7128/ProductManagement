e11fe54bc6d32b1af13a8c80da4878df9882cfd6
vidyasagar gaikwad <vidyasagargaikwad7128@gmail.com>
2021-10-21 17:42:34 +0530

    Added: UC4&UC5 Count & ProductId Reviews

--- ProductManagement/Program.cs
+++ ProductManagement/Program.cs
@@ -8,28 +8,44 @@ namespace ProductManagement
         static void Main(string[] args)
         {
             Console.WriteLine("Product Management!");
+            int count = 0;
             List<Product> products = new List<Product>()
             {
                 new Product(){ProductID = 1, UserID = 1, Rating = 3.9, Review = "Amazing", isLike = true},
                 new Product(){ProductID = 2, UserID = 1, Rating = 3.4, Review = "Good", isLike = false},
                 new Product(){ProductID = 3, UserID = 3, Rating = 4.0, Review = "Nice", isLike = false},
                 new Product(){ProductID = 4, UserID = 4, Rating = 2.9, Review = "Its Good", isLike = true},
-                new Product(){ProductID = 5, UserID = 5, Rating = 5.0, Review = "Amazing", isLike = true},
-                new Product(){ProductID = 6, UserID = 6, Rating = 3.9, Review = "Amazing", isLike = true},
-                new Product(){ProductID = 7, UserID = 3, Rating = 3.4, Review = "Good", isLike = false},
-                new Product(){ProductID = 8, UserID = 8, Rating = 4.0, Review = "Nice", isLike = false},
-                new Product(){ProductID = 9, UserID = 10, Rating = 4.5, Review = "Its Good", isLike = true},
-                new Product(){ProductID = 10, UserID = 9, Rating = 5.0, Review = "Amazing", isLike = true},
+                new Product(){ProductID = 5, UserID = 5, Rating = 5.0, Review = "Fine", isLike = true},
+                new Product(){ProductID = 6, UserID = 6, Rating = 3.9, Review = "Not Bad", isLike = true},
+                new Product(){ProductID = 7, UserID = 3, Rating = 3.4, Review = ";)", isLike = false},
+                new Product(){ProductID = 8, UserID = 8, Rating = 4.0, Review = "Cool", isLike = true},
+                new Product(){ProductID = 9, UserID = 10, Rating = 4.5, Review = "Just Ok", isLike = true},
+                new Product(){ProductID = 10, UserID = 9, Rating = 5.0, Review = null, isLike = true},
             };
+            ///Top 3 Reviews 
             var topThree = (from pro in products
                                 where pro.Rating > 3.0 && pro.ProductID == 1 || pro.Rating > 3.0 && pro.ProductID == 4 || pro.Rating > 3.0 && pro.ProductID == 9
                             orderby pro.Rating descending
                                 select pro);
-
             foreach (var p in topThree)
             {
                 Console.WriteLine($"{p.ProductID} {p.UserID} {p.Rating} {p.Review} {p.isLike}");
             }
+            ///reviews Count
+            var reviewCounts = (
+                from size in products
+                    orderby size.Review
+                    select size);
+            foreach(var i in reviewCounts)
+            {
+                if (i.Review != null)
+                {
+                    count++;
+                    Console.WriteLine($"ProductId: {i.ProductID} Review: {i.Review}");
+                }
+                    
+            }
+            Console.WriteLine($"{count} Reviews by Users");
         }
     }
 }
